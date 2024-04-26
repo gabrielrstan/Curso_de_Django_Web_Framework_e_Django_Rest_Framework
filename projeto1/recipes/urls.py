6ce7aab272dd1d59e58e6fe659950b6e304031c8
gabrielrstan <51632457+gabrielrstan@users.noreply.github.com>
2024-04-26 17:49:44 -0300

    Iniciado e terminado a seção 7

--- projeto1/recipes/urls.py
+++ projeto1/recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from recipes.views import home
+from recipes.views import home, recipe
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('recipes/<int:id>/', recipe, name='recipe'),
 
 ]
