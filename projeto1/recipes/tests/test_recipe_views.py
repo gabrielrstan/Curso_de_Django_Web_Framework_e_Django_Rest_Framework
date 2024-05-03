450d34c90f08f09f5e4569ca2a3bda337e46bc82
gabrielrstan <51632457+gabrielrstan@users.noreply.github.com>
2024-05-03 20:58:19 -0300

    Inicio da seção 10

--- projeto1/recipes/tests/test_recipe_views.py
+++ projeto1/recipes/tests/test_recipe_views.py
@@ -1,7 +1,7 @@
 from django.urls import resolve, reverse
 
 from recipes.tests.test_recipe_base import RecipeTestBase
-from recipes.views import category, home, recipe
+from recipes.views import category, home, recipe, search
 
 
 class RecipeViewsTest(RecipeTestBase):
@@ -99,4 +99,17 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': recipe.pk}))
 
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_uses_correct_view_function(self):
+        resolved = resolve(reverse('recipes:search'))
+        self.assertIs(resolved.func, search)
+
+    def test_recipe_search_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:search') + '?q=test')
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
--- projeto1/recipes/urls.py
+++ projeto1/recipes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from recipes.views import category, home, recipe
+from recipes.views import category, home, recipe, search
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('recipes/search/', search, name='search'),
     path('recipes/category/<int:category_id>/', category, name='category'),
     path('recipes/<int:id>/', recipe, name='recipe'),
 
