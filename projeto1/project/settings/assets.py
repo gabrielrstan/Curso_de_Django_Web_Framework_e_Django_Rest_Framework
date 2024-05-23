6b1ce4cc99ee99d6e7fe8f485dfdb804beec1b7f
gabrielrstan <51632457+gabrielrstan@users.noreply.github.com>
2024-05-23 16:07:32 -0300

    Seção 20, aula 253, Organizando setting.py em um Python Package

---
+++ projeto1/project/settings/assets.py
@@ -0,0 +1,13 @@
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+from .environment import BASE_DIR
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
