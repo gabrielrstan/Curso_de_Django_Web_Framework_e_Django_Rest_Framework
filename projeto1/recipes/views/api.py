8360dc312ccba5a0fff2e181c4fb0efcd0d30a93
gabrielrstan <51632457+gabrielrstan@users.noreply.github.com>
2024-05-29 12:05:54 -0300

    Termino da seção 25

--- projeto1/recipes/views/api.py
+++ projeto1/recipes/views/api.py
@@ -19,26 +19,49 @@ def recipe_api_list(request):
         )
         return Response(serializer.data)
     elif request.method == 'POST':
-        serializer = RecipeSerializer(data=request.data)
+        serializer = RecipeSerializer(
+            data=request.data,
+            context={'request': request},
+        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data,
-                        status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
     return Response('Error')
 
 
-@api_view()
+@api_view(['get', 'patch', 'delete'])
 def recipe_api_detail(request, pk):
     recipe = get_object_or_404(
         Recipe.objects.get_published(),  # type:ignore
         pk=pk,
     )
-    serializer = RecipeSerializer(
-        instance=recipe,
-        context={'request': request},
-    )
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            context={'request': request},
+        )
+        return Response(serializer.data)
+
+    elif request.method == 'PATCH':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            partial=True,
+            context={'request': request},
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    return Response('Error')
 
 
 @api_view()
